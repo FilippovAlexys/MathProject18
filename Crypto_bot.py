79a9c0475dfbc700d9e3876c00a5f4690e8bd188
Alexey <alexys_fss@mail.ru>
2021-10-07 10:56:35 +0300

    v2

--- Crypto_bot.py
+++ Crypto_bot.py
@@ -14,11 +14,11 @@ def helps(message: telebot.types.Message):
 
 
 @bot.message_handler(commands=['values'])
-def values(mesage: telebot.types.Message):
+def values(message: telebot.types.Message):
     text = 'Доступные валюты:'
     for key in keys.keys():
         text = '\n'.join((text, key,))
-    bot.reply_to(mesage, text)
+    bot.reply_to(message, text)
 
 
 @bot.message_handler(content_types=['text', ])
